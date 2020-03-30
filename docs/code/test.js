ddde0b9e09230d5e705c1c16de8508710bc02b14
luvsunlight <516060659@qq.com>
2020-03-31 00:01:13 +0800

    update doc

--- docs/code/test.js
+++ docs/code/test.js
@@ -1,10 +1,20 @@
-function jumpFloorII(number) {
-	if (number <= 1) return number
-	let hashmap = { 1: 1 }
-	for (let i = 2; i <= number; i++) {
-		hashmap[i] = hashmap[i - 1] * 2
+/* function TreeNode(x) {
+    this.val = x;
+    this.left = null;
+    this.right = null;
+} */
+function HasSubtree(p1, p2) {
+	function isSubTree(t1, t2) {
+		if (!p2) return true
+		if (!p1) return false
+		return (
+			t1.val === t2.val &&
+			isSameTree(t1.left, t2.left) &&
+			isSameTree(t1.right, t2.right)
+		)
 	}
-	return hashmap[number]
+	if (!p1 || !p2) return false
+	return (
+		isSubTree(p1, p2) || HasSubtree(p1.left, p2) || HasSubtree(p1.right, p2)
+	)
 }
-
-console.log(jumpFloorII(5))
