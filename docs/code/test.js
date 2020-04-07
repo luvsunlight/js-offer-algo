7ab7d8e7a1729fe4c2a6ae3e2b54e8cd5b17e926
luvsunlight <516060659@qq.com>
2020-04-07 14:19:35 +0800

    add 35~45

--- docs/code/test.js
+++ docs/code/test.js
@@ -1,10 +1,16 @@
-function InversePairs(data) {
-	let res = 0
-	for (let i = 0; i < data.length; i++) {
-		for (let j = i + 1; j < data.length; j++) {
-			if (data[i] > data[j]) res++
+function IsContinuous(numbers) {
+	let set = new Set()
+	let max = -Infinity,
+		min = Infinity
+	for (let i = 0; i < numbers.length; i++) {
+		let num = numbers[i]
+		if (num !== 0) {
+			if (set.has(num)) return false
+			set.add(num)
+			if (num > max) max = num
+			if (num < min) min = num
 		}
 	}
-	return res
+	return max - min <= 4
 }
-console.log(InversePairs([1, 2, 3, 4, 5]))
+console.log(IsContinuous([1, 3, 2, 5, 4]))
