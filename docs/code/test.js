56a9430f0f140ef51030f82dd4890e325e1527e6
luvsunlight <516060659@qq.com>
2020-04-14 15:02:56 +0800

    complete 46~67

--- docs/code/test.js
+++ docs/code/test.js
@@ -1,16 +1,15 @@
-function IsContinuous(numbers) {
-	let set = new Set()
-	let max = -Infinity,
-		min = Infinity
-	for (let i = 0; i < numbers.length; i++) {
-		let num = numbers[i]
-		if (num !== 0) {
-			if (set.has(num)) return false
-			set.add(num)
-			if (num > max) max = num
-			if (num < min) min = num
+function NumberOf1Between1AndN_Solution(n) {
+	if (n < 1) return 0
+	let counts = 0,
+		num = 0
+	for (let i = 1; i <= n; i++) {
+		num = i
+		while (num > 0) {
+			if (num % 10 == 1) {
+				counts++
+			}
+			num = Math.floor(num / 10)
 		}
 	}
-	return max - min <= 4
+	return counts
 }
-console.log(IsContinuous([1, 3, 2, 5, 4]))
